e74110ce89e17551222bac1cd32a0025a412ef28
Felipe <126893383+feoliveira7@users.noreply.github.com>
2024-04-09 23:09:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// function toggleText(event){
+
+//     var text = event.textContext || event.innerText;
+//     if(text == 'Criar minha conta!')
+//     {
+//         event.innerHTML = 'Conta criada com sucesso!';
+//     }
+ 
+// }
